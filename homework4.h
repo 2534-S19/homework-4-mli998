47a3000ada66ff923ff482b2e8e29d1efe171ceb
Meijin <mli9@vt.edu>
2020-04-09 22:19:27 -0400

    Homework4

--- homework4.h
+++ homework4.h
@@ -1,8 +1,8 @@
 /*
  * homework4.h
  *
- *  Created on:
- *      Author:
+ *  Created on:4/7/2020
+ *      Author:Meijin Li
  */
 
 #ifndef HOMEWORK4_H_
@@ -13,6 +13,12 @@ void initBoard();
 bool charFSM(char rChar);
 
 // TODO: Define any constants that are local to homework.c using #define
-
-
+   #define LAUNCHPAD_ONLY
+   #define P2_RED_LED_H GPIO_PIN0
+   #define P2_BLUE_LED_H GPIO_PIN2
+   #define P2_GREEN_LED_H GPIO_PIN1
+   #define P2_ALL_LEDS_H (P2_RED_LED_H|P2_BLUE_LED_H|P2_GREEN_LED_H)
+   #define A0_BRDIV 1
+   #define A0_UCxBRF 10
+   #define A0_UCxBRS 0
 #endif /* HOMEWORK4_H_ */
